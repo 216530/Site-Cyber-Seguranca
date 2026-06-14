35ba279790b79b57f7c6a72321cdf25732872be6
208992-ui <208992@upf.br>
2026-06-14 15:31:29 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
-// Comportamento: ao clicar em "Enviar", dispara um alerta com a mensagem: "Página em construção".
-document.getElementById("submitButton").addEventListener("click", function(event) {
-    event.preventDefault(); // parar o submit do form
-    alert("Página em construção");
-});
+var tam = 16;
+function aumentarFonte() {
+    tam = tam + 1; 
+    document.body.style.fontSize = tam + "px";
+}
+function diminuirFonte() {
+    if (tam > 10) { 
+        tam = tam - 1; 
+        document.body.style.fontSize = tam+ "px";
+    }
+}
+function alternarTema() {
+    var corpoPagina = document.body;
+    var btnTema = document.querySelector(".btnTema");
+    corpoPagina.classList.toggle("modoNoturnoFull");
+    
+    if (btnTema) {
+        if (corpoPagina.classList.contains("modoNoturnoFull")) {
+            btnTema.innerText = "Modo Claro";
+        } else {
+            btnTema.innerText = "Modo Dark";
+        }
+    }
+}
+
+
+/* tive que mudar essa parte de JS pq tava quebrando o resto kkkkk*/
+window.onload = function() {
+    var botaoSubmit = document.getElementById("submitButton");
+    if (botaoSubmit) {
+        botaoSubmit.addEventListener("click", function(event) {
+            event.preventDefault(); 
+            alert("Página em construção");
+        });
+    }
+};
